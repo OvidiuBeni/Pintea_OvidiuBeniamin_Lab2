fc8ce2cec76cf5e00759586f704ba7dddf489d04
OvidiuBeni <ovidiu_beni@yahoo.com>
2023-10-21 19:02:12 +0300

    Add project files.

---
+++ Data/Pintea_OvidiuBeniamin_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Pintea_OvidiuBeniamin_Lab2.Models;
+
+namespace Pintea_OvidiuBeniamin_Lab2.Data
+{
+    public class Pintea_OvidiuBeniamin_Lab2Context : DbContext
+    {
+        public Pintea_OvidiuBeniamin_Lab2Context (DbContextOptions<Pintea_OvidiuBeniamin_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Pintea_OvidiuBeniamin_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Pintea_OvidiuBeniamin_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
